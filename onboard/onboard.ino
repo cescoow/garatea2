aa9d76f53bf69df7d1ee7a0989f968004fada53c
danilobso <danilobso@gmail.com>
2016-10-28 20:31:43 -0200

    Port do GPS pro arduino + POO.

--- onboard/onboard.ino
+++ onboard/onboard.ino
@@ -1,6 +1,8 @@
 #include "DHT.h"
 #include "BUZZ.hpp"
 #include "SERVO.h"
+#include "GPS.h"
+#include <avr/wdt.h> // Biblioteca utilizada pelo watchdog timer.
 
 // Pin Define
 #define DHTPIN 2
@@ -14,24 +16,33 @@ DHT dht(DHTPIN, DHTTYPE);
 BUZZ buzzer(BUZZPIN);
 SERVO myServo(SERVOPIN);
 
+float hic;
+float h;
+float t;
+
 void setup(){
- Serial.begin(9600); 
- dht.begin();
+
+    Serial.begin(9600); 
+    dht.begin();
+    wdt_enable(WDTO_4S); // Computador ira reiniciar se o timer nao for resetado em 4s
 }
 
 void loop(){
-    delay(2000);
-    float h = dht.readHumidity();
-    float t = dht.readTemperature();
-    float hic = dht.computeHeatIndex(t, h, false);
 
-      Serial.print("\nHumidity: ");
-      Serial.print(h);
-      Serial.print("\nTemperature: ");
-      Serial.print(t);
-      Serial.print(" *C ");
-      Serial.print("\nHeat index: ");
-      Serial.print(hic);
-      Serial.print(" *C ");
+    wdt_reset(); // Funcao que reseta o timer
+
+    delay(2000);
+    h = dht.readHumidity();
+    t = dht.readTemperature();
+    hic = dht.computeHeatIndex(t, h, false);
+
+    Serial.print("\nHumidity: ");
+    Serial.print(h);
+    Serial.print("\nTemperature: ");
+    Serial.print(t);
+    Serial.print(" *C ");
+    Serial.print("\nHeat index: ");
+    Serial.print(hic);
+    Serial.print(" *C ");
 }
 
