aeb564dd10ced39882ed22389f3a0b61293bd71f
cescoow <francescorossilena@gmail.com>
2017-10-07 17:25:13 -0300

    GRTA 3 code - 2° review 2017

---
+++ Revisão 2 semestre/código final/master/master.ino
@@ -0,0 +1,271 @@
+#include "hBMP.h"
+#include "hDHT.h"
+#include <Wire.h>
+#include <string.h>
+#include <SoftwareSerial.h>
+#include <avr/wdt.h>
+#include "TinyGPS++.h"
+int flag = 0;
+
+String stringdata1 = "";
+String stringdata2 = "";
+String stringdata3 = "";
+String stringdata4 = "";
+String stringdata5 = "";
+
+static const uint32_t GPSBaud = 9600;
+
+TinyGPSPlus gps;
+
+SoftwareSerial ss(3 , 4);
+SoftwareSerial ubk(10 , 11);
+
+hDHT _dht(2, DHT22);
+hBMP bmp;
+bool _f1, _f2, _f3, _f4;
+String _B1, _B2;
+
+void requestEvent() {
+  //Serial.print(stringdata1);
+  // Serial.print(stringdata2);
+	
+	//Serial.println(F("Requisicao recebida."));
+  
+	if(_f1){
+   Serial.print(stringdata1);
+	 Serial.println(F("B1 requisitada."));
+   char chardata1[32];
+   stringdata1.toCharArray(chardata1, 32);
+	 Wire.write(chardata1);
+   //Wire.endTransmission();
+	}
+  wdt_reset();
+	if(_f2){
+    Serial.print(stringdata2);
+		Serial.println(F("B2 requisitada."));
+    char chardata2[32];
+    stringdata2.toCharArray(chardata2, 32);
+		Wire.write(chardata2);
+    //Wire.endTransmission();
+	}
+  if(_f3){
+    Serial.print(stringdata3);
+    Serial.println(F("B3 requisitada."));
+    char chardata3[32];
+    stringdata3.toCharArray(chardata3, 32);
+    Wire.write(chardata3);
+    //Wire.endTransmission();
+  }
+   if(_f4){
+    Serial.print(stringdata4);
+    Serial.println(F("B4 requisitada."));
+    char chardata4[32];
+    stringdata4.toCharArray(chardata4, 32);
+    Wire.write(chardata4);
+    //Wire.endTransmission();
+  }
+	
+	if(_f1){
+		_f1 = false;
+    _f2 = true;
+    _f3 = false;
+    _f4 = false;
+	} else if(_f2){
+    _f1 = false;
+    _f2 = false;
+    _f3 = true;
+    _f4 = false;
+	} else if(_f3){
+    _f1 = false;
+    _f2 = false;
+    _f3 = false;
+    _f4 = true;
+	} else if(_f4){
+    _f1 = true;
+    _f2 = false;
+    _f3 = false;
+    _f4 = false;
+ }
+
+	Serial.println(F("Enviado"));
+  digitalWrite(7, HIGH);  
+  delay(200);              
+  digitalWrite(7, LOW);       
+  stringdata1 = "";
+  stringdata2 = "";
+  stringdata3 = "";
+  stringdata4 = "";
+}
+
+
+
+void setup(){
+  pinMode(7, OUTPUT);
+  Serial.begin(115200);
+  ss.begin(GPSBaud);
+  ubk.begin(GPSBaud);
+  ubk.listen();
+  Serial.print(F("Testing TinyGPS++ library v. ")); Serial.println(TinyGPSPlus::libraryVersion());
+  wdt_enable(WDTO_8S);
+	Serial.begin(115200);
+	Serial.println("Setup incializado.");
+
+	_dht.begin();
+    Serial.println(F("DHT incializado."));
+
+	Wire.begin(94);  //endereço i2c             
+	Wire.onRequest(requestEvent);
+
+	_f1 = true;
+	Serial.println("Setup finalizado.");
+
+	wdt_reset();
+}
+
+
+void loop(){
+
+  static const double Home_LAT = -22.000818, Home_LON = -47.901968;
+
+  unsigned long distanceKmToHome =
+    (unsigned long)TinyGPSPlus::distanceBetween(
+      gps.location.lat(),
+      gps.location.lng(),
+      Home_LAT, 
+      Home_LON) / 1000;
+
+  double courseToLondon =
+    TinyGPSPlus::courseTo(
+      gps.location.lat(),
+      gps.location.lng(),
+      Home_LAT, 
+      Home_LON);
+
+
+  const char *cardinalToLondon = TinyGPSPlus::cardinal(courseToLondon);
+
+
+  //Serial.println("string:  ");
+  //stringdata1.concat(gps.time);stringdata1.concat(";"); // arrumar 
+  if (flag == 0){
+    if (gps.altitude.isUpdated()||gps.hdop.isUpdated() || gps.location.isUpdated() || gps.satellites.isUpdated() || gps.course.isUpdated() || gps.speed.isUpdated()){
+      stringdata1 = "";
+      stringdata2 = "";
+      stringdata4 = "";
+      stringdata5 = "";
+      stringdata1.concat("u");stringdata1.concat(";");    
+      stringdata1.concat(gps.hdop.value());stringdata1.concat(";"); //saude do fix
+      stringdata1.concat((gps.location.lat()*10000));stringdata1.concat(";");
+      stringdata1.concat(gps.location.lng()*10000);stringdata1.concat(";");
+      stringdata1.concat(gps.satellites.value());stringdata1.concat(";");
+      stringdata2.concat(gps.altitude.meters());stringdata2.concat(";");
+      stringdata2.concat(gps.course.deg());stringdata2.concat(";");
+      stringdata2.concat(gps.speed.kmph());stringdata2.concat(";");
+      stringdata2.concat(distanceKmToHome);stringdata2.concat(";");
+      stringdata2.concat(courseToLondon);stringdata2.concat(";");
+      //stringdata2.concat(cardinalToLondon);stringdata2.concat(";");
+      //stringdata2.concat(TinyGPSPlus::cardinal(gps.course.value()));stringdata2.concat("\n");
+
+      stringdata5.concat(_dht.getTemp());stringdata5.concat(";");
+      stringdata5.concat(_dht.getHumd());stringdata5.concat(";");
+      
+
+  
+      flag = 1;
+   } 
+  }
+
+   if (flag == 1){ 
+    if (gps.altitude.isUpdated()||gps.hdop.isUpdated() || gps.location.isUpdated() || gps.satellites.isUpdated() || gps.course.isUpdated() || gps.speed.isUpdated()){
+      stringdata3 = "";
+      stringdata4 = "";
+      stringdata5 = "";
+      stringdata3.concat("a");stringdata3.concat(";");    
+      stringdata3.concat(gps.hdop.value());stringdata3.concat(";"); //saude do fix
+      stringdata3.concat((gps.location.lat()*10000));stringdata3.concat(";");
+      stringdata3.concat(gps.location.lng()*10000);stringdata3.concat(";");
+      stringdata3.concat(gps.satellites.value());stringdata3.concat(";");
+      stringdata4.concat(gps.altitude.meters());stringdata4.concat(";");
+      stringdata4.concat(gps.course.deg());stringdata4.concat(";");
+      stringdata4.concat(gps.speed.kmph());stringdata4.concat(";");
+      stringdata4.concat(distanceKmToHome);stringdata4.concat(";");
+      stringdata4.concat(courseToLondon);stringdata4.concat(";");
+      //stringdata4.concat(cardinalToLondon);stringdata4.concat(";");
+      //stringdata4.concat(TinyGPSPlus::cardinal(gps.course.value()));stringdata4.concat("\n");
+
+      stringdata5.concat(_dht.getTemp());stringdata5.concat(";");
+      stringdata5.concat(_dht.getHumd());stringdata5.concat(";");
+      
+
+      
+      flag = 0;
+   } 
+  }
+  
+  
+
+  // gps.location.isValid() retorna true se tem fix - Seria legal implementar
+  smartDelay(1000); // Mantem a comunicação ativa até que não tenha mais pacotes. Não alterar
+
+  if (millis() > 5000 && gps.charsProcessed() < 10)
+    Serial.println(F("No GPS data received: check wiring")); // colocar alarme sonoro
+  
+	//Serial.println(F("Esperando requisicao."));
+	wdt_reset();
+}
+
+
+
+// As funções para o Ublox
+
+static void smartDelay(unsigned long ms)
+{
+  unsigned long start = millis();
+  do 
+  {
+    if (flag == 0){
+    ubk.listen();
+    while (ubk.available() > 0){
+      gps.encode(ubk.read());
+    } 
+  }else{
+    ss.listen();
+    while (ss.available() > 0){
+      gps.encode(ss.read());
+    }
+  }
+      
+  } while (millis() - start < ms);
+}
+
+
+
+static void printDateTime(TinyGPSDate &d, TinyGPSTime &t) 
+{
+  if (!d.isValid())
+  {
+    Serial.print(F("********** "));
+  }
+  else
+  {
+    char sz[32];
+    sprintf(sz, "%02d/%02d/%02d ", d.month(), d.day(), d.year());
+    Serial.print(sz);
+  }
+  
+  if (!t.isValid())
+  {
+    Serial.print(F("******** "));
+  }
+  else
+  {
+    char sz[32];
+    sprintf(sz, "%02d:%02d:%02d ", t.hour(), t.minute(), t.second());
+    Serial.print(sz);
+  }
+
+  smartDelay(0);
+}
+
+
+
---
+++ Revisão 2 semestre/código final/slave/slave_-_lora.ino
@@ -0,0 +1,146 @@
+#include <Wire.h>
+#include <SPI.h>
+#include <SD.h>
+#include <avr/wdt.h>
+#include <LoRa.h>
+
+#include <stdlib.h>
+#include <string.h>
+#include <util/crc16.h>
+#define RADIOPIN 5
+int counter = 0;
+int i = 0;
+String stringdata = "";
+char _B[20];
+File myFile;
+
+void radioSetup(){
+	pinMode(RADIOPIN, OUTPUT);
+	pinMode(A1, OUTPUT);
+	//digitalWrite(A1, HIGH); 
+}
+
+void setup(){  
+ Serial.begin(115200);
+ Wire.begin();
+ LoRa.setPins(7, A0, 2);
+ if (!LoRa.begin(433E6)) {
+    Serial.println("Starting LoRa failed!");
+    while (1);
+  }
+  
+	
+	while (!SD.begin(4)) {
+    	Serial.println("Falha no SD.");
+    	delay(1000);
+  	}
+  	Serial.println("SD inicializado.");
+
+  	_B[0] = '\0';
+
+  	radioSetup();
+  	wdt_enable(WDTO_8S);
+}
+
+
+void save_data(){
+	int time = millis();
+	wdt_reset(); 
+	Serial.println(F("Save."));
+	myFile = SD.open("log.txt", FILE_WRITE);
+	if(myFile){
+		myFile.print(time);
+		myFile.print(';');
+		myFile.println(_B);
+		myFile.close();
+		Serial.println(F("Gravado."));
+	}
+}
+
+void receive_data(){
+	wdt_reset(); 
+	Serial.println(F("Receive."));
+	char c;
+	_B[0] = '\0';
+	int i = 0;
+
+	Wire.requestFrom(94, 32);    // 6 bytes do endereço #8
+ 	while (Wire.available()) { // slave may send less than requested
+    char c = Wire.read(); // receive a byte as character
+    //Serial.print(c);         // print the character
+    stringdata.concat(c);
+    
+  }
+  Serial.println(stringdata);
+  save_data();
+}
+
+void rtty_txbit (int bit) {
+  if (bit) {
+    digitalWrite(5, HIGH);
+  }
+  else {
+    digitalWrite(5, LOW);
+  }
+
+  delayMicroseconds(10000);
+  delayMicroseconds(10150); 
+}
+
+void rtty_txbyte (char c) {
+	int i;
+	rtty_txbit (0);
+	for (i = 0; i < 7; i++) { // Change this here 7 or 8 for ASCII-7 / ASCII-8
+		if (c & 1) rtty_txbit(1);
+		else rtty_txbit(0);
+		c = c >> 1;
+	}
+	rtty_txbit (1); // Stop bit
+	rtty_txbit (1); // Stop bit
+	wdt_reset();
+}
+
+void rtty_txstring (char * string) {
+	char c;
+	c = *string++;
+	while ( c != '\0') {
+		rtty_txbyte (c);
+		c = *string++;
+	}
+}
+
+uint16_t gps_CRC16_checksum (char *string) {
+  size_t i;
+  uint16_t crc;
+  uint8_t c;
+  crc = 0xFFFF;
+
+  for (i = 2; i < strlen(string); i++) {
+    c = string[i];
+    crc = _crc_xmodem_update (crc, c);
+  }
+  return crc;
+}
+
+void loop(){
+	receive_data();
+	wdt_reset(); 
+	//rtty_txstring("hi");
+	wdt_reset();
+	delay(1000);
+  Serial.print("Sending packet: ");
+  Serial.println(counter);
+
+  // send packet
+  LoRa.beginPacket();
+  LoRa.print(stringdata);
+  LoRa.print(counter);
+  LoRa.endPacket();
+  counter++;
+  delay(1000);
+  i++;
+  if (i >=5 ){
+    stringdata = "";
+    i = 0;
+  }
+}
